43866abba0791d9d6aab2e8fb705c959f3a8195a
Ezmana <ezmana.land@gmail.com>
2019-04-30 14:28:45 +0200

    Organization of the files

---
+++ NetSetMan/tools/chocolateyinstall.ps1
@@ -0,0 +1,11 @@
+﻿$ErrorActionPreference = 'Stop';
+$url        = 'https://www.netsetman.com/download.php'
+$packageName = 'netsetman'
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'NetSetMan*'
+  silentArgs   = '/SILENT'
+}
+Install-ChocolateyPackage @packageArgs
