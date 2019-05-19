7839c60d142d13996227a19d8811a0cd9b701c8b
Ezmana <ezmana.land@gmail.com>
2019-05-19 12:28:38 +0200

    New package YaTQA

---
+++ YaTQA/Source/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop'; 
+$url        	= 'https://dl.yat.qa/stable/YaTQA-Setup_3.9.7.exe' 
+$packageName	= 'yatqa'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'YaTQA*'
+  checksum      = '329F4C1E3528CA07D4646C930B4457122238553C6267E5CCF1026A9FA1EA2ACE'
+  checksumType  = 'sha256'
+  validExitCodes= @(0)
+  silentArgs   = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
+Install-ChocolateyShortcut -ShortcutFilePath "C:\Users\Public\Desktop\YaTQA.lnk" -TargetPath "C:\Program Files (x86)\YaTQA\yatqa.exe"
---
+++ YaTQA/Source/tools/chocolateyuninstall.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName  = 'yatqa'
+$shortcutName = 'YaTQA.lnk'
+
+Uninstall-BinFile yatqa
+remove-item "$ENV:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
+remove-item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
+remove-item "C:\Program Files (x86)\YaTQA" -Force -ErrorAction SilentlyContinue
