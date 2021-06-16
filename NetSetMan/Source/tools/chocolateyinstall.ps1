9c704baa16a2682332e4af2dd3a10ed616d3be7d
3zmana <ezmana.land@gmail.com>
2021-06-16 18:08:31 +0200

    Update 5.0.6

--- NetSetMan/Source/tools/chocolateyinstall.ps1
+++ NetSetMan/Source/tools/chocolateyinstall.ps1
@@ -6,7 +6,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url           = $url
   softwareName  = 'NetSetMan*'
-  checksum      = 'F8E70D4665DC5280FC7636A031065E028AC33729F7FF48785D7E061F3DC56D6F'
+  checksum      = '7BE7431DF66052EF3BC2E8C64F6712918C20C0E959B3567A468A39AF3CA165ED'
   checksumType  = 'sha256'
   silentArgs   = '/SILENT'
 }
