51aa5be1d7d7f383adf7e65d1cce14bc75ba9d9c
Mitch Talmadge <8848080+MitchTalmadge@users.noreply.github.com>
2022-06-13 19:11:20 -0600

    Remove YaTQA
    Maybe it'll return in the future.

--- YaTQA/Source/tools/chocolateyuninstall.ps1
+++
@@ -1,7 +0,0 @@
-﻿$ErrorActionPreference = 'Stop'
-$packageName  = 'yatqa'
-$shortcutName = 'YaTQA.lnk'
-
-remove-item "$ENV:Public\Desktop\$shortcutName" -Force -ErrorAction SilentlyContinue
-remove-item "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$shortcutName" -Force -ErrorAction SilentlyContinue
-remove-item "C:\Program Files (x86)\YaTQA" -Force -ErrorAction SilentlyContinue
